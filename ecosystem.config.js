1a8c4fa1ad79415b0d4c0d2292230db7d52431f2
ErnisuuluRenat <rinatovich124@gmail.com>
2023-04-24 15:37:18 +0600

    new ecosystem upd 2.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,20 @@
-export const apps = [
-      {
-            name: "vite-project",
-            script: "yarn",
-            args: "vite build && serve -s build -l 3000",
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: "1G",
-            cwd: '/Programmer/clientSide/mebel_02',
-            env: {
-                  NODE_ENV: 'production',
-                  HOST: '46.8.43.42',
-                  PORT: 3000,
-            },
-      },
-];
-       
+module.exports = {
+      apps: [
+        {
+          name: "vite-project",
+          script: "yarn",
+          args: "vite build && serve -s build -l 3000",
+          instances: 1,
+          autorestart: true,
+          watch: false,
+          max_memory_restart: "1G",
+          cwd: "/Programmer/clientSide/mebel_02",
+          env: {
+            NODE_ENV: "production",
+            HOST: "0.0.0.0",
+            PORT: 3000
+          }
+        }
+      ]
+    };
+    
